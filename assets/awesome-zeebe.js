30784538514f51c22c8038ed5b189cffa628ad50
Sebastian Menski <sebastian.menski@camunda.com>
2018-12-18 16:27:46 +0100

    chore(page): add error message if github api requests are exhausted

--- assets/awesome-zeebe.js
+++ assets/awesome-zeebe.js
@@ -1,3 +1,5 @@
+var errorMsg = '<h2 style="text-align: center">Failed to fetch the awesome-zeebe list. Please visit <a href="https://github.com/zeebe-io/awesome-zeebe">https://github.com/zeebe-io/awesome-zeebe</a></h2>';
+
 var getFileData = function(url) {
   $.ajax({
     url: url,
@@ -5,20 +7,24 @@ var getFileData = function(url) {
       "Accept": "application/vnd.github.v3.raw"
     }
   }).done(function(data) {
-    // var converter = new showdown.Converter({literalMidWordUnderscores: true});
-    // var html = converter.makeHtml(data);
     $("#content").html(marked(data));
   })
+        .fail(function() {
+            $("#content").html(errorMsg);
+        });
 };
 
 $(document).ready(function(){
-  $.getJSON("https://api.github.com/repos/zeebe-io/awesome-zeebe/git/trees/HEAD").
-    done(function(data){
+  $.getJSON("https://api.github.com/repos/zeebe-io/awesome-zeebe/git/trees/HEAD")
+    .done(function(data){
       for (var i = data.tree.length - 1; i >= 0; i--) {
         if(data.tree[i].path == "README.md") {
           getFileData(data.tree[i].url);
           break;
         }
       };
+    })
+    .fail(function() {
+        $("#content").html(errorMsg);
     });
 });
